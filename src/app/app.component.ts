c29e3988118dde4f5a6f8f60ab1db098f1c7b01c
Maiia <katerinadribnitsa@gmail.com>
2018-11-12 17:26:14 +0200

    loader added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from './shared/services/loader.service';
 
 @Component({
   selector: 'bb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'binck';
 
-  constructor() {}
+  constructor() // private loaderService: LoaderService
+  {
+    // this.loaderService.show();
+  }
+
+  ngOnInit() {}
 }
